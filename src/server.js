e18dc78a4cd2abf22c7a9283b3646931e0f0331f
Lina <linaskrobot@gmail.com>
2026-01-10 21:54:03 +0000

    Fix: add default PORT value 3000

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import pino from 'pino-http';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
