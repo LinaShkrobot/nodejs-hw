23db2245b9094bfab7bc3c4d46afc58edf449a5f
Lina <linaskrobot@gmail.com>
2026-01-18 00:05:40 +0000

    changes errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,9 +1,15 @@
+import { isHttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  const status = err.status || 500;
+  if (isHttpError(err)) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
 
-  res.status(status).json({
-    message: err.message,
+  res.status(500).json({
+    message: err.message || 'Something went wrong',
   });
 };
